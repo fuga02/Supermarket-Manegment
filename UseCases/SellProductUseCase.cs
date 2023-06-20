1cb8120661a03906223734feee331cce31e6c018
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-06-19 23:21:56 -0700

    Worked on Cashier's console

---
+++ UseCases/SellProductUseCase.cs
@@ -0,0 +1,22 @@
+﻿using UseCases.DataStorePluginInterfaces;
+using UseCases.UseCaseInterfaces;
+
+namespace UseCases;
+
+public class SellProductUseCase: ISellProductUseCase
+{
+    private readonly IProductRepository _productRepository;
+
+    public SellProductUseCase(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public void Execute(int productId, int? qytToSell)
+    {
+        var product = _productRepository.GetProductById(productId);
+        if (product == null) { return;}
+        product.Quantity -= qytToSell;
+        _productRepository.UpdateProduct(product);
+    }
+}
